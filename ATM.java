3828912509ef771b8dd347ac6ff2a2f1525f6571
saumyabhan <133961694+saumyabhan@users.noreply.github.com>
2023-09-14 22:20:09 +0530

    Add files via upload

---
+++ ATM.java
@@ -0,0 +1,71 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ATM {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BankAccount account = new BankAccount();
+
+        while (true) {
+            System.out.println("Welcome to the ATM!");
+            System.out.println("1. Withdraw");
+            System.out.println("2. Deposit");
+            System.out.println("3. Check Balance");
+            System.out.println("4. Exit");
+            System.out.print("Please select an option: ");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter the amount to withdraw: ");
+                    double withdrawAmount = scanner.nextDouble();
+                    if (account.withdraw(withdrawAmount)) {
+                        System.out.println("Withdrawal successful.");
+                    } else {
+                        System.out.println("Insufficient funds.");
+                    }
+                    break;
+                case 2:
+                    System.out.print("Enter the amount to deposit: ");
+                    double depositAmount = scanner.nextDouble();
+                    account.deposit(depositAmount);
+                    System.out.println("Deposit successful.");
+                    break;
+                case 3:
+                    System.out.println("Your balance is: " + account.checkBalance());
+                    break;
+                case 4:
+                    System.out.println("Thank you for using the ATM!");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        }
+    }
+}
+
+class BankAccount {
+    private double balance;
+
+    public BankAccount() {
+        this.balance = 0.0;
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public double checkBalance() {
+        return balance;
+    }
+}
